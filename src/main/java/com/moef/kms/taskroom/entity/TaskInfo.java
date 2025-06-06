188b184a35b265c18ac6e1046dd2a1cbed1930ec
HeounTo <lookmeplz@naver.com>
2025-06-07 01:20:26 +0900

    feat- entitiy

--- src/main/java/com/moef/kms/taskroom/entity/TaskInfo.java
+++ src/main/java/com/moef/kms/taskroom/entity/TaskInfo.java
@@ -1,20 +1,37 @@
 package com.moef.kms.taskroom.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 @Data
 public class TaskInfo {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long taskId;
+
+    @Column(length = 20, nullable = false)
     private String userId;
+
+    @Column(length = 20, nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private LocalDate startDate;
+
+    @Column
     private LocalDate endDate;
+
+    @Column
     private boolean alarmSet;
+
+    @Column
     private boolean visibleSet;
 }
